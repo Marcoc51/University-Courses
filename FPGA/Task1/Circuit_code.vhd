ab29feb5fa0200539e25d9f2e3735d9144ba6c01
Marc Atef <64839030+Marcoc51@users.noreply.github.com>
2022-05-15 22:30:26 +0200

    Add files via upload

---
+++ FPGA/Task1/Circuit_code.vhd
@@ -0,0 +1,108 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    00:31:52 05/15/2022 
+-- Design Name: 
+-- Module Name:    des - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity my_circ is
+    Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
+           B : in STD_LOGIC_VECTOR (7 downto 0);
+           op_cel : in STD_LOGIC_VECTOR (3 downto 0);
+           c_in : in STD_LOGIC_VECTOR (0 downto 0);
+           Disp_0 : out STD_LOGIC_VECTOR (6 downto 0);
+           Disp_1 : out STD_LOGIC_VECTOR (6 downto 0));
+end my_circ;
+
+architecture Behavioral of my_circ is
+Signal result: STD_LOGIC_VECTOR (7 downto 0);
+Signal bcd_0, bcd_1: std_logic_vector(3 downto 0);
+
+
+begin
+
+    With op_cel select
+result <= std_logic_vector(unsigned(A) + unsigned(B)) when "0001",
+                 std_logic_vector(unsigned(A) - unsigned(B))  when "0010",
+                 A AND B  when "0011",
+                 A OR B when "0100",
+                 std_logic_vector(unsigned( NOT B) +1) when "0101",  
+                 std_logic_vector(unsigned( NOT A) + 1) when "0111",
+                 std_logic_vector(unsigned(A) + 1) when "1000",
+                 A NOR B when "1001",
+                 std_logic_vector(unsigned(A) + unsigned(B) + unsigned(c_in)) when "1010",
+	     (others => '0') when others;
+--  result <= "00110011";
+bcd_1 <= result(7) & result(6) & result(5) & result(4);
+bcd_0 <= result(3) & result(2) & result(1) & result(0);
+
+
+with bcd_1 select
+Disp_1 <= "1111110" when "0000",
+        "0110000" when "0001", 
+        "1101101" when "0010", 
+        "1111001" when "0011", 
+        "0110011" when "0100", 
+        "1011011" when "0101", 
+        "1011111" when "0110", 
+        "1110000" when "0111", 
+        "1111111" when "1000", 
+        "1111011" when "1001", 
+        "1111101" when "1010",
+         "0011111" when "1011",
+         "1001110" when "1100",
+         "0111101" when "1101", 
+         "1001111" when "1110", 
+         "1000111" when "1111", 
+        (others => '0') when others;
+        
+        
+        with bcd_0 select
+        Disp_0 <= "1111110" when "0000",
+                "0110000" when "0001", 
+                "1101101" when "0010", 
+                "1111001" when "0011", 
+                "0110011" when "0100", 
+                "1011011" when "0101", 
+                "1011111" when "0110", 
+                "1110000" when "0111", 
+                "1111111" when "1000", 
+                "1111011" when "1001", 
+                "1111101" when "1010",
+               "0011111" when "1011",
+                "1001110" when "1100",
+                "0111101" when "1101", 
+                "1001111" when "1110", 
+                "1000111" when "1111", 
+                (others => '0') when others;
+        
+
+
+
+end Behavioral;
+
+
---
+++ FPGA/Task1/Test_Bench_code.vhd
@@ -0,0 +1,118 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   01:28:48 05/15/2022
+-- Design Name:   
+-- Module Name:   D:/New folder/hhh/testy.vhd
+-- Project Name:  hhh
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: my_circ
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY testy IS
+END testy;
+ 
+ARCHITECTURE behavior OF testy IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT my_circ
+    PORT(
+         A : IN  std_logic_vector(7 downto 0);
+         B : IN  std_logic_vector(7 downto 0);
+         op_cel : IN  std_logic_vector(3 downto 0);
+         c_in : IN  std_logic_vector(0 downto 0);
+         Disp_0 : OUT  std_logic_vector(6 downto 0);
+         Disp_1 : OUT  std_logic_vector(6 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic_vector(7 downto 0) := (others => '0');
+   signal B : std_logic_vector(7 downto 0) := (others => '0');
+   signal op_cel : std_logic_vector(3 downto 0) := (others => '0');
+   signal c_in : std_logic_vector(0 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal Disp_0 : std_logic_vector(6 downto 0);
+   signal Disp_1 : std_logic_vector(6 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: my_circ PORT MAP (
+          A => A,
+          B => B,
+          op_cel => op_cel,
+          c_in => c_in,
+          Disp_0 => Disp_0,
+          Disp_1 => Disp_1
+        );
+
+   -- Clock process definitions
+   process
+   begin
+	A <= "00001010";
+B <= "01010010";
+c_in <= "1";
+
+op_cel <= "0000";
+wait for 100 ns;
+
+op_cel <= "0001";
+wait for 100 ns;
+
+op_cel <= "0010";
+wait for 100 ns;
+
+op_cel <= "0011";
+wait for 100 ns;
+
+op_cel <= "0100";
+wait for 100 ns;
+
+op_cel <= "0101";
+wait for 100 ns;
+
+op_cel <= "0111";
+wait for 100 ns;
+
+op_cel <= "1000";
+wait for 100 ns;
+
+op_cel <= "1001";
+wait for 100 ns;
+
+   end process;
+ 
+
+  
+
+END;
